19f9b665f38722854e9913728f8f8c7e3464b536
Michael Gelfand <mgelfa@student.ubc.ca>
2025-02-27 14:08:24 -0800

    Create initial app for purposes of deployment.

---
+++ src/app.py
@@ -0,0 +1,36 @@
+import altair as alt
+import dash_bootstrap_components as dbc
+import dash_vega_components as dvc
+from dash import Dash, dcc, callback, Input, Output
+import geopandas as gpd
+
+
+url = 'https://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_1_states_provinces.zip'
+world_regions = gpd.read_file(url)[['wikipedia', 'name', 'region', 'admin', 'postal', 'latitude', 'longitude', 'geometry']]
+
+# Initiatlize the app
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+# Layout
+app.layout = dbc.Container([
+    dcc.Dropdown(id='country', value='India', options=world_regions['admin'].unique()),
+    dvc.Vega(id='map', spec={}),
+])
+
+# Server side callbacks/reactivity
+@callback(
+    Output('map', 'spec'),
+    Input('country', 'value'),
+)
+def create_map(country):
+    return alt.Chart(world_regions.query(f'admin == "{country}"'), width=600, height=500).mark_geoshape(stroke='white').encode(
+        tooltip='name',
+        color=alt.Color('name').scale(scheme='tableau20'),  # To avoid repeating colors
+    ).to_dict()
+
+
+
+# Run the app/dashboard
+if __name__ == '__main__':
+    server.run(debug=True)
