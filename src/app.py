c2e21610b11d456fdb00e502fb1a432821020383
Michael Gelfand <48497591+michael-gelfand@users.noreply.github.com>
2025-02-27 19:32:32 -0800

    Add initial map graphic including loading the necessary GeoJSON data (#53)
    * Add initial map graphic including loading the necessary GeoJSON data
    
    * Fixes to initial map to get it working.
    
    * Change map data to group by boroughs instead of by precinct

--- src/app.py
+++ src/app.py
@@ -1,38 +1,111 @@
 import altair as alt
 import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
-from dash import Dash, dcc, callback, Input, Output
+from dash import Dash, dcc, callback, Input, Output, html
 import geopandas as gpd
+import pandas as pd
 
-# This code is adapted from the 532 lecture 4 course notes. This is an initial version just
-# so we can get the deployment up and running. This code will be replaced with our actual
-# code in the future.
+# Load the NYC borough GeoJSON data
+borough_url = "https://raw.githubusercontent.com/codeforgermany/click_that_hood/refs/heads/main/public/data/new-york-city-boroughs.geojson"
+nyc_boroughs = gpd.read_file(borough_url)
 
-url = 'https://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_1_states_provinces.zip'
-world_regions = gpd.read_file(url)[['wikipedia', 'name', 'region', 'admin', 'postal', 'latitude', 'longitude', 'geometry']]
+# Load the NYPD Precinct GeoJSON data
+precinct_url = "https://raw.githubusercontent.com/ResidentMario/geoplot-data/refs/heads/master/nyc-police-precincts.geojson"
+nyc_precinct = gpd.read_file(precinct_url)
+
+# Load the NYPD Arrests data
+arrest_path = "data/raw/NYPD_Arrest_Data__Year_to_Date_.csv"
+nyc_arrests = pd.read_csv(arrest_path)
+
+# Perform data wrangling to tie arrest data to precinct and borough
+# GeoJSON data.
+nyc_arrests = nyc_arrests[nyc_arrests["ARREST_PRECINCT"] != 483]
+
+# Map borough names to borough codes
+borough_mapping = {
+    'B': 'Bronx',
+    'S': 'Staten Island',
+    'K': 'Brooklyn',
+    'M': 'Manhattan',
+    'Q': 'Queens'
+}
+nyc_arrests['borough'] = nyc_arrests['ARREST_BORO'].map(borough_mapping)
+
+nyc_precinct['precinct'] = pd.to_numeric(
+    nyc_precinct['precinct'],
+    downcast='integer',
+    errors='coerce'
+)
+
+# Merge arrest data with GeoJSON data
+arrest_data_geo = nyc_precinct.merge(
+    nyc_arrests,
+    how="left",
+    left_on="precinct",
+    right_on="ARREST_PRECINCT"
+)
+
+# Grouped data for initial visualization
+arrest_data_grouped_geo = arrest_data_geo.groupby(
+    "borough"
+).size().reset_index(name='counts')
+
+# Merge grouped data with GeoJSON data
+nyc_full = arrest_data_grouped_geo.merge(
+    nyc_boroughs,
+    how="left",
+    left_on="borough",
+    right_on="name"
+)
+
+# Select columns
+nyc_full = nyc_full[
+    ["borough", "counts", "geometry"]
+]
+
+# Rename columns
+nyc_full = nyc_full.rename(
+    columns={'borough': 'Borough', 'counts': 'Arrests'}
+)
+
+# Convert GeoDataFrame to DataFrame
+nyc_full["geojson"] = nyc_full["geometry"].apply(lambda x: x.__geo_interface__)
+
+# Normalize the GeoJSON data
+geo_data = pd.json_normalize(nyc_full["geojson"])
+geo_data["Borough"] = nyc_full["Borough"]
+geo_data["Arrests"] = nyc_full["Arrests"]
 
 # Initiatlize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
+# Create map altair object
+map_chart = alt.Chart(
+    geo_data,
+    width=600,
+    height=500
+).mark_geoshape(
+    stroke='grey'
+).project(
+    'albersUsa'
+).encode(
+    color='Arrests',
+    tooltip=['Borough', alt.Tooltip('Arrests', format=',')]
+).to_dict()
+
 # Layout
 app.layout = dbc.Container([
-    dcc.Dropdown(id='country', value='India', options=world_regions['admin'].unique()),
-    dvc.Vega(id='map', spec={}),
+    html.H1("NYPD Arrests Map"),
+    dbc.Row([
+        dbc.Col(
+            [
+                dvc.Vega(spec=map_chart)
+            ],
+        md=8
+        )
+    ])
 ])
-
-# Server side callbacks/reactivity
-@callback(
-    Output('map', 'spec'),
-    Input('country', 'value'),
-)
-def create_map(country):
-    return alt.Chart(world_regions.query(f'admin == "{country}"'), width=600, height=500).mark_geoshape(stroke='white').encode(
-        tooltip='name',
-        color=alt.Color('name').scale(scheme='tableau20'),  # To avoid repeating colors
-    ).to_dict()
-
-
 
 # Run the app/dashboard
 if __name__ == '__main__':
