afd094da3d623e72009deb45d6af95fdc15ceef0
Michael Gelfand <mgelfa@student.ubc.ca>
2025-03-08 16:00:25 -0800

    Add helper function to create empty pie chart for empty dataframes.

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,5 +2,7 @@ from .helpers import (
     filter_data_by_crime_type,
     get_selected_location,
     filter_data_by_location,
-    create_pie_chart
+    create_pie_chart,
+    filter_data_by_date_range,
+    create_empty_pie_chart
 )
