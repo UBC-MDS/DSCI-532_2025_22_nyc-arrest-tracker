b62bb43757e6ced976e0b09d701bdc12816dee20
Michael Gelfand <mgelfa@student.ubc.ca>
2025-03-09 16:15:34 -0700

    Remove modebar from bar and pie charts

--- src/components/charts.py
+++ src/components/charts.py
@@ -6,17 +6,20 @@ from src.utils import create_pie_chart, create_bar_chart
 # Crime frequency pie chart
 crime_bar_chart = dcc.Graph(
     id='crime-bar-chart',
+    config={'displayModeBar': False},
     figure=create_bar_chart(crime_pie_data.head(5), "Top 5 Crime Types")
 )
 
 # Gender distribution pie chart
 gender_pie_chart = dcc.Graph(
     id='gender-pie-chart',
+    config={'displayModeBar': False},
     figure=create_pie_chart(gender_data, "Arrests by Gender")
 )
 
 # Age distribution pie chart
 age_pie_chart = dcc.Graph(
     id='age-pie-chart',
+    config={'displayModeBar': False},
     figure=create_pie_chart(age_data, "Arrests by Age Group")
 )
