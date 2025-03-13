34e41598f7b1f758e1e8d480d15138483a53ddf0
Michael Hewlett <37633154+mdahewlett@users.noreply.github.com>
2025-03-13 15:08:13 -0700

    feat: make about section a toggle (#138)

--- src/callbacks/buttons.py
+++ src/callbacks/buttons.py
@@ -34,3 +34,22 @@ def reset_date_range(n_clicks):
         return dash.no_update, dash.no_update
     
     return min_date, max_date
+
+@callback(
+    Output("footer-collapse", "is_open"),
+    Input("footer-toggle-button", "n_clicks"),
+    State("footer-collapse", "is_open"),
+)
+def toggle_footer(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+@callback(
+    Output("footer-toggle-button", "children"),
+    Input("footer-collapse", "is_open"),
+)
+def update_footer_button_text(is_open):
+    if is_open:
+        return "About ▲" 
+    return "About ▼" 
--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,3 +1,3 @@
-from .general import title_comp, collapse_button, sidebar, footer_info
+from .general import title_comp, collapse_button, sidebar, footer_component
 from .charts import crime_bar_chart, gender_pie_chart, age_pie_chart
 from .map import map_chart
