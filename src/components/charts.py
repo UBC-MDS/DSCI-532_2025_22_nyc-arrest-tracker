09d745741f58398358fac70b36c50f37d841eba8
Michael Gelfand <48497591+michael-gelfand@users.noreply.github.com>
2025-03-13 15:35:36 -0700

    Feat add loading indicators (#142)
    * feat: Add loading indicators to pie/bar charts
    
    * feat: Add loading indicator to map

--- src/components/charts.py
+++ src/components/charts.py
@@ -4,22 +4,28 @@ from src.data import crime_pie_data, gender_data, age_data
 from src.utils import create_pie_chart, create_bar_chart
 
 # Crime frequency pie chart
-crime_bar_chart = dcc.Graph(
-    id='crime-bar-chart',
-    config={'displayModeBar': False},
-    figure=create_bar_chart(crime_pie_data.head(5), "Top 5 Crime Types")
+crime_bar_chart = dcc.Loading(
+    children=[dcc.Graph(
+        id='crime-bar-chart',
+        config={'displayModeBar': False},
+        figure=create_bar_chart(crime_pie_data.head(5), "Top 5 Crime Types")
+    )]
 )
 
 # Gender distribution pie chart
-gender_pie_chart = dcc.Graph(
-    id='gender-pie-chart',
-    config={'displayModeBar': False},
-    figure=create_pie_chart(gender_data, "Arrests by Gender")
+gender_pie_chart = dcc.Loading(
+    children=[dcc.Graph(
+        id='gender-pie-chart',
+        config={'displayModeBar': False},
+        figure=create_pie_chart(gender_data, "Arrests by Gender")
+    )]
 )
 
 # Age distribution pie chart
-age_pie_chart = dcc.Graph(
-    id='age-pie-chart',
-    config={'displayModeBar': False},
-    figure=create_pie_chart(age_data, "Arrests by Age Group")
+age_pie_chart = dcc.Loading(
+    children=[dcc.Graph(
+        id='age-pie-chart',
+        config={'displayModeBar': False},
+        figure=create_pie_chart(age_data, "Arrests by Age Group")
+    )]
 )
