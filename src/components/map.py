564b4e12f4183d91d167e8bbf067e3e5ba5def7c
Michael Gelfand <mgelfa@student.ubc.ca>
2025-03-13 15:32:17 -0700

    feat: Add loading indicator to map

--- src/components/map.py
+++ src/components/map.py
@@ -1,11 +1,14 @@
 import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
+from dash import dcc
 
 map_chart = dbc.Col(
-    dvc.Vega(
-        id='map',
-        spec={},
-        signalsToObserve=['select_region']
+    dcc.Loading(
+        children=[dvc.Vega(
+            id='map',
+            spec={},
+            signalsToObserve=['select_region']
+        )]
     ),
     md=8
 )
