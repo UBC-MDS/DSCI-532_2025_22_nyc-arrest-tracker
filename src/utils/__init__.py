4f85fcf66ee088cb9b6d71ec517cabc19eb4a3c6
Michael Hewlett <37633154+mdahewlett@users.noreply.github.com>
2025-03-13 17:09:50 -0700

    refactor: looked for row-wise ops and other code to optimize (#144)
    * optimizer helper filters
    
    * feat: optimize charts
    
    * feat: optimize map callback
    
    * feat: optimize data
    
    * fix: add legend back to left

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,10 @@
 from .helpers import (
-    filter_data_by_crime_type,
+    filter_data,
     get_selected_location,
     filter_data_by_location,
     create_pie_chart,
     create_bar_chart,
+    filter_data_by_crime_type,
     create_empty_bar_chart,  
     create_empty_pie_chart,
     filter_data_by_date_range
